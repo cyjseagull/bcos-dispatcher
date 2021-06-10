61dfe70571f2e0e4eaf64f1c1332796409e5adfa
morebtcg <651932351@qq.com>
2021-06-10 18:58:23 +0800

    add code

--- bcos-dispatcher/DispatcherImpl.cpp
+++ bcos-dispatcher/DispatcherImpl.cpp
@@ -1,1 +1,20 @@
 #include "DispatcherImpl.h"
+#include <thread>
+
+using namespace bcos::dispatcher;
+
+void DispatcherImpl::asyncExecuteBlock(const protocol::Block::Ptr &_block, bool _verify,
+                                       std::function<void(const Error::Ptr &, const protocol::BlockHeader::Ptr &)> _callback) {
+  m_queue.emplace(_block->blockHeader()->number(), BlockWithCallback({_block, _verify, _callback}));
+}
+void DispatcherImpl::asyncGetLatestBlock(std::function<void(const Error::Ptr &, const protocol::Block::Ptr &)> _callback) {
+    auto item = m_queue.begin();
+    if(item == m_queue.end()) {
+        // std::thread::hardware_concurrency()
+    }
+}
+
+void DispatcherImpl::asyncNotifyExecutionResult(const Error::Ptr &_error, const protocol::BlockHeader::Ptr &_header,
+                                                std::function<void(const Error::Ptr &)> _callback) {}
+void DispatcherImpl::start() {}
+void DispatcherImpl::stop() {}
--- bcos-dispatcher/DispatcherImpl.h
+++ bcos-dispatcher/DispatcherImpl.h
@@ -1,9 +1,33 @@
 #pragma once
 
+#include <gsl/span>
+#include <tbb/concurrent_queue.h>
+#include "bcos-framework/interfaces/dispatcher/DispatcherInterface.h"
+#include "bcos-framework/interfaces/executor/ExecutorInterface.h"
+
 namespace bcos {
 namespace dispatcher {
-    class DispatcherImpl  {
+class DispatcherImpl : public DispatcherInterface {
+public:
+  ~DispatcherImpl() override {}
+  void asyncExecuteBlock(const protocol::Block::Ptr &_block, bool _verify,
+                         std::function<void(const Error::Ptr &, const protocol::BlockHeader::Ptr &)> _callback) override;
+  void asyncGetLatestBlock(std::function<void(const Error::Ptr &, const protocol::Block::Ptr &)> _callback) override;
 
-    };
-}
+  void asyncNotifyExecutionResult(const Error::Ptr &_error, const protocol::BlockHeader::Ptr &_header,
+                                  std::function<void(const Error::Ptr &)> _callback) override;
+  void start() override;
+  void stop() override;
+
+private:
+  struct BlockWithCallback {
+    protocol::Block::Ptr block;
+    bool verify;
+    std::function<void(const Error::Ptr &, const protocol::BlockHeader::Ptr &)> callback;
+  };
+
+  std::shared_ptr<bcos::executor::ExecutorInterface> m_executor;
+  std::map<bcos::protocol::BlockNumber, BlockWithCallback> m_queue;
+};
+} // namespace dispatcher
 } // namespace bcos
