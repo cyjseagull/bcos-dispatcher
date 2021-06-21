2903b298ec15ac4e96d4934ee4bf3cccbd489082
cyjseagull <1547158861@qq.com>
2021-06-21 16:09:29 +0800

    modify the code-style (#4)

--- bcos-dispatcher/DispatcherImpl.cpp
+++ bcos-dispatcher/DispatcherImpl.cpp
@@ -3,57 +3,71 @@
 
 using namespace bcos::dispatcher;
 
-void DispatcherImpl::asyncExecuteBlock(const protocol::Block::Ptr &_block, bool _verify,
-                                       std::function<void(const Error::Ptr &, const protocol::BlockHeader::Ptr &)> _callback) {
-  auto item = BlockWithCallback({_block, _verify, _callback});
-  std::function<void(const Error::Ptr &, const protocol::Block::Ptr &)> callback;
+void DispatcherImpl::asyncExecuteBlock(const protocol::Block::Ptr& _block, bool _verify,
+    std::function<void(const Error::Ptr&, const protocol::BlockHeader::Ptr&)> _callback)
+{
+    auto item = BlockWithCallback({_block, _verify, _callback});
+    std::function<void(const Error::Ptr&, const protocol::Block::Ptr&)> callback;
 
-  tbb::mutex::scoped_lock scoped(m_mutex);
-  auto result = m_waitingQueue.try_pop(callback);
-  if (result) {
-    m_number2Callback.emplace(item.block->blockHeader()->number(), item);
-    scoped.release();
-    callback(nullptr, item.block);
-  } else {
-    m_blockQueue.emplace(BlockWithCallback({_block, _verify, _callback}));
-  }
-}
-
-void DispatcherImpl::asyncGetLatestBlock(std::function<void(const Error::Ptr &, const protocol::Block::Ptr &)> _callback) {
-  BlockWithCallback item;
-
-  tbb::mutex::scoped_lock scoped(m_mutex);
-  auto result = m_blockQueue.try_pop(item);
-  if (result) {
-    m_number2Callback.emplace(item.block->blockHeader()->number(), item);
-    scoped.release();
-    _callback(nullptr, item.block);
-  } else {
-    m_waitingQueue.push(_callback);
-  }
-}
-
-void DispatcherImpl::asyncNotifyExecutionResult(const Error::Ptr &_error, const protocol::BlockHeader::Ptr &_header,
-                                                std::function<void(const Error::Ptr &)> _callback) {
-  {
     tbb::mutex::scoped_lock scoped(m_mutex);
-    auto it = m_number2Callback.find(_header->number());
-    if (it != m_number2Callback.end()) {
-      auto item = it->second;
-      m_number2Callback.erase(it);
-
-      scoped.release();
-
-      item.callback(_error, _header);
-    } else {
-      auto error = std::make_shared<bcos::Error>(-1, "No such block: " + boost::lexical_cast<std::string>(_header->number()));
-      _callback(error);
-
-      return;
+    auto result = m_waitingQueue.try_pop(callback);
+    if (result)
+    {
+        m_number2Callback.emplace(item.block->blockHeader()->number(), item);
+        scoped.release();
+        callback(nullptr, item.block);
     }
-  }
+    else
+    {
+        m_blockQueue.emplace(BlockWithCallback({_block, _verify, _callback}));
+    }
+}
 
-  _callback(nullptr);
+void DispatcherImpl::asyncGetLatestBlock(
+    std::function<void(const Error::Ptr&, const protocol::Block::Ptr&)> _callback)
+{
+    BlockWithCallback item;
+
+    tbb::mutex::scoped_lock scoped(m_mutex);
+    auto result = m_blockQueue.try_pop(item);
+    if (result)
+    {
+        m_number2Callback.emplace(item.block->blockHeader()->number(), item);
+        scoped.release();
+        _callback(nullptr, item.block);
+    }
+    else
+    {
+        m_waitingQueue.push(_callback);
+    }
+}
+
+void DispatcherImpl::asyncNotifyExecutionResult(const Error::Ptr& _error,
+    const protocol::BlockHeader::Ptr& _header, std::function<void(const Error::Ptr&)> _callback)
+{
+    {
+        tbb::mutex::scoped_lock scoped(m_mutex);
+        auto it = m_number2Callback.find(_header->number());
+        if (it != m_number2Callback.end())
+        {
+            auto item = it->second;
+            m_number2Callback.erase(it);
+
+            scoped.release();
+
+            item.callback(_error, _header);
+        }
+        else
+        {
+            auto error = std::make_shared<bcos::Error>(
+                -1, "No such block: " + boost::lexical_cast<std::string>(_header->number()));
+            _callback(error);
+
+            return;
+        }
+    }
+
+    _callback(nullptr);
 }
 
 void DispatcherImpl::start() {}
--- tests/testDispatcher.cpp
+++ tests/testDispatcher.cpp
@@ -2,80 +2,91 @@
 #include "bcos-framework/interfaces/protocol/Block.h"
 #include "bcos-framework/interfaces/protocol/BlockHeader.h"
 #include "bcos-framework/interfaces/protocol/ProtocolTypeDef.h"
-#include "bcos-framework/testutils/protocol/FakeBlockHeader.h"
 #include "bcos-framework/testutils/protocol/FakeBlock.h"
+#include "bcos-framework/testutils/protocol/FakeBlockHeader.h"
+#include <bcos-framework/interfaces/crypto/CryptoSuite.h>
 #include <bcos-framework/testutils/crypto/HashImpl.h>
 #include <bcos-framework/testutils/crypto/SignatureImpl.h>
-#include <bcos-framework/interfaces/crypto/CryptoSuite.h>
-#include <boost/test/tools/old/interface.hpp>
-#include <boost/test/unit_test.hpp>
 #include <tbb/parallel_for.h>
 #include <tbb/parallel_invoke.h>
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
 
-namespace bcos {
-namespace test {
-struct DispatcherFixture {
-  DispatcherFixture() { dispatcher = std::make_shared<dispatcher::DispatcherImpl>(); }
+namespace bcos
+{
+namespace test
+{
+struct DispatcherFixture
+{
+    DispatcherFixture() { dispatcher = std::make_shared<dispatcher::DispatcherImpl>(); }
 
-  dispatcher::DispatcherImpl::Ptr dispatcher;
+    dispatcher::DispatcherImpl::Ptr dispatcher;
 };
 
 BOOST_FIXTURE_TEST_SUITE(TestDispatcher, DispatcherFixture)
 
-BOOST_AUTO_TEST_CASE(queue) {
-  tbb::atomic<size_t> receiveCount = 0;
-  tbb::atomic<size_t> sendCount = 0;
+BOOST_AUTO_TEST_CASE(queue)
+{
+    tbb::atomic<size_t> receiveCount = 0;
+    tbb::atomic<size_t> sendCount = 0;
 
-  auto hashImpl = std::make_shared<Keccak256Hash>();
-  auto signatureImpl = std::make_shared<Secp256k1SignatureImpl>();
-  auto cryptoSuite = std::make_shared<CryptoSuite>(hashImpl, signatureImpl, nullptr);
-  auto blockFactory = createBlockFactory(cryptoSuite);
+    auto hashImpl = std::make_shared<Keccak256Hash>();
+    auto signatureImpl = std::make_shared<Secp256k1SignatureImpl>();
+    auto cryptoSuite = std::make_shared<CryptoSuite>(hashImpl, signatureImpl, nullptr);
+    auto blockFactory = createBlockFactory(cryptoSuite);
 
-  std::random_device rng;
-  std::uniform_int_distribution<> during(0, 1000);
+    std::random_device rng;
+    std::uniform_int_distribution<> during(0, 1000);
 
 
-  tbb::parallel_invoke(
-      [this, &sendCount, &during, &rng, cryptoSuite, blockFactory]() {
-        for (size_t i = 100; i < 2000; ++i) {
-          usleep(during(rng));
+    tbb::parallel_invoke(
+        [this, &sendCount, &during, &rng, cryptoSuite, blockFactory]() {
+            for (size_t i = 100; i < 2000; ++i)
+            {
+                usleep(during(rng));
 
-          auto testBlock = fakeAndCheckBlock(cryptoSuite, blockFactory, true, 0, 0);
-          testBlock->blockHeader()->setNumber(i);
-          // sim push tx
-          dispatcher->asyncExecuteBlock(testBlock, false, [testBlock, &sendCount](const Error::Ptr &error, const protocol::BlockHeader::Ptr &block) {
-            (void)error;
-            BOOST_CHECK_EQUAL(testBlock->blockHeader()->number(), block->number());
-            ++sendCount;
-          });
-        }
-      },
-      [this, &receiveCount, &during, &rng]() {
-        for (size_t i = 100; i < 2000; ++i) {
-          usleep(during(rng));
-          dispatcher->asyncGetLatestBlock([this, &receiveCount, i, &during, &rng](const Error::Ptr &, const protocol::Block::Ptr &block) {
-            BOOST_CHECK_EQUAL(block->blockHeader()->number(), i);
-            ++receiveCount;
+                auto testBlock = fakeAndCheckBlock(cryptoSuite, blockFactory, true, 0, 0);
+                testBlock->blockHeader()->setNumber(i);
+                // sim push tx
+                dispatcher->asyncExecuteBlock(testBlock, false,
+                    [testBlock, &sendCount](
+                        const Error::Ptr& error, const protocol::BlockHeader::Ptr& block) {
+                        (void)error;
+                        BOOST_CHECK_EQUAL(testBlock->blockHeader()->number(), block->number());
+                        ++sendCount;
+                    });
+            }
+        },
+        [this, &receiveCount, &during, &rng]() {
+            for (size_t i = 100; i < 2000; ++i)
+            {
+                usleep(during(rng));
+                dispatcher->asyncGetLatestBlock(
+                    [this, &receiveCount, i, &during, &rng](
+                        const Error::Ptr&, const protocol::Block::Ptr& block) {
+                        BOOST_CHECK_EQUAL(block->blockHeader()->number(), i);
+                        ++receiveCount;
 
-            // sim get and run tx
-            usleep(during(rng));
-            dispatcher->asyncNotifyExecutionResult(nullptr, block->blockHeader(), [](const Error::Ptr &error) { BOOST_CHECK(!error); });
-          });
-        }
-      });
+                        // sim get and run tx
+                        usleep(during(rng));
+                        dispatcher->asyncNotifyExecutionResult(nullptr, block->blockHeader(),
+                            [](const Error::Ptr& error) { BOOST_CHECK(!error); });
+                    });
+            }
+        });
 
-  dispatcher->asyncGetLatestBlock([](const Error::Ptr &, const protocol::Block::Ptr &) { 
-    BOOST_FAIL("Expect to be empty"); 
-    });
+    dispatcher->asyncGetLatestBlock(
+        [](const Error::Ptr&, const protocol::Block::Ptr&) { BOOST_FAIL("Expect to be empty"); });
 
-  auto testBlockHeader = testPBBlockHeader(cryptoSuite);
-  testBlockHeader->setNumber(500);
-  dispatcher->asyncNotifyExecutionResult(nullptr, testBlockHeader, [](const Error::Ptr &error) { BOOST_CHECK_EQUAL(error->errorCode(), -1); });
+    auto testBlockHeader = testPBBlockHeader(cryptoSuite);
+    testBlockHeader->setNumber(500);
+    dispatcher->asyncNotifyExecutionResult(nullptr, testBlockHeader,
+        [](const Error::Ptr& error) { BOOST_CHECK_EQUAL(error->errorCode(), -1); });
 
-  BOOST_CHECK_EQUAL(receiveCount, sendCount);
+    BOOST_CHECK_EQUAL(receiveCount, sendCount);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-} // namespace test
-} // namespace bcos
+}  // namespace test
+}  // namespace bcos
